0b02b10d42e6a80ad6a57db018a05765e923483d
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2023-11-07 11:18:50 +0100

    Add public factory classes
    Make factory functions importable from `s7.factories` as `create_*`,
    where * matches the name of any `*_factory.py` file in the folder.

---
+++ s7/factories/__init__.py
@@ -0,0 +1,7 @@
+"""Public factory functions for creating SOFT7-related classes and instances."""
+from .datasource_factory import create_datasource
+from .dataspace_factory import class_factory as create_dataspace
+from .graph_factory import create_outer_entity as create_graph
+
+
+__all__ = ("create_datasource", "create_dataspace", "create_graph")
