2a5f8d561b348248716f2067790e5fba261d5296
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2024-05-27 09:49:01 +0200

    Add docs deployment workflow
    Remove using Make to build documentation - instead just use
    'sphinx-build' straight up.

--- docs/conf.py
+++ docs/conf.py
@@ -29,3 +29,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
+html_theme_options = {
+    "navigation_with_keys": True,
+}
