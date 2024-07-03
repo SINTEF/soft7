6270ed1f6926197aea8ecfe36d0f099efba97006
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2024-07-03 13:18:03 +0200

    Revert "Minor style fix"
    This reverts commit f0531a1f7b4db7da40f199eaf023c422674d6628.

--- s7/factories/__init__.py
+++ s7/factories/__init__.py
@@ -5,4 +5,7 @@ from __future__ import annotations
 from .datasource_factory import create_datasource
 from .entity_factory import create_entity
 
-__all__ = ("create_datasource", "create_entity")
+__all__ = (
+    "create_datasource",
+    "create_entity",
+)
